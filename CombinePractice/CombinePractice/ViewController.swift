fb2cbac84224848dcecfb943cb33e872ccb1437d
Denis <recmanus@gmail.com>
2022-02-07 10:55:54 +0300

    Feature/Lesson2_Stage1
    Add filters in accordance with the stage 1 of Home Work.

--- CombinePractice/CombinePractice/ViewController.swift
+++ CombinePractice/CombinePractice/ViewController.swift
@@ -11,26 +11,44 @@ import Combine
 class ViewController: UIViewController {
     
     private var cancellables = Set<AnyCancellable>()
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // MARK: Example One
         
-        let publisherLessonOne = NotificationCenter.default.publisher(for: Notification.testNotificationLessonOne, object: nil)
-        let subscriberLessonOne = publisherLessonOne.sink { notification in
-            print("Received \(notification.name.rawValue)")
-        }
-        NotificationCenter.default.post(name: Notification.testNotificationLessonOne, object: nil, userInfo: nil)
-        subscriberLessonOne.cancel()
+        //        // MARK: Example One
+        //
+        //        let publisherLessonOne = NotificationCenter.default.publisher(for: Notification.testNotificationLessonOne, object: nil)
+        //        let subscriberLessonOne = publisherLessonOne.sink { notification in
+        //            print("Received \(notification.name.rawValue)")
+        //        }
+        //        NotificationCenter.default.post(name: Notification.testNotificationLessonOne, object: nil, userInfo: nil)
+        //        subscriberLessonOne.cancel()
+        //
+        //        // MARK: Example Two
+        //
+        //        let publisherTwo = ["Hello Combine"].publisher
+        //        let subscriberTwo = publisherTwo.sink { receivedValue in
+        //            print("Received from publisherTwo: ", receivedValue)
+        //        }
+        //        subscriberTwo.cancel()
         
-        // MARK: Example Two
+        //MARK: Lesson 2 Home work
         
-        let publisherTwo = ["Hello Combine"].publisher
-        let subscriberTwo = publisherTwo.sink { receivedValue in
-            print("Received from publisherTwo: ", receivedValue)
-        }
-        subscriberTwo.cancel()
+        //Lesson 2 Stage 1
+        
+        let publisherStageOne = (1...100).publisher
+        publisherStageOne
+            .filter { $0 > 49 && $0 < 71 }
+            .filter { $0 % 2 == 0 }
+            .collect()
+            .sink(receiveValue: { numbers in
+                print(numbers)
+            })
+            .cancel()
+        
+        //Lesson 2 Stage 2
+        
     }
     
 }
